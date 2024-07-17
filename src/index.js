50f6100c8878be5e53f14bd47845d23e73daf93a
Had1na <gadubas@gmail.com>
2024-07-17 16:02:09 +0100

    Added home link

--- src/index.js
+++ src/index.js
@@ -42,7 +42,8 @@ function updateCity(event) {
         <div class="time">${cityTime.format(
           "h:mm:ss [<small>]A[</small>]"
         )}</div>
-        </div>`;
+        </div>
+        <a href="/">Home</a>`;
 }
 
 updateTime();
